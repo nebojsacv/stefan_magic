21f6ed16fefbbaea72926898ce1b66d2a44dbcf4
stefanrakic <stefanrakic92@gmail.com>
2026-02-20 17:55:39 +0100

    Sql fix length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Observers\UserObserver;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Fix for MySQL utf8mb4 key length issue
+        Schema::defaultStringLength(191);
+
         // Register User Observer
         User::observe(UserObserver::class);
     }
