3300a49d4d68f7a18600bc3180e6b6acf80f0c0b
stefanrakic <stefanrakic92@gmail.com>
2026-03-12 22:47:28 +0100

    Fix manual vendor classification

--- app/Models/VendorClassification.php
+++ app/Models/VendorClassification.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class VendorClassification extends Model
 {
-    //
+    protected $fillable = [
+        'vendor_id',
+        'risk_level',
+        'classification_method',
+        'criticality_score',
+        'data_access_level',
+        'dependency_level',
+        'questionnaire_answers',
+        'classified_by',
+        'approved_by',
+        'approved_at',
+        'notes',
+    ];
 }
